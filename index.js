eae9809effd4d6ae0b8dbf363fa1434324d382ed
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-08-08 23:19:43 +0100

    implement 'more' function

--- index.js
+++ index.js
@@ -5,21 +5,39 @@ var ee           = new EventEmitter();
 
 function hotukdeals(){
     EventEmitter.call(this);
-    this.items = [];
     this.name = 'hotukdeals.com';
+    this.baseUrl = 'http://wwww.hotukdeals.com';
+
+    // set inside `parse`
+    this.items = [];
+    this._html = null;
+    this.$ = null;
+    this.currentPage = null;
 };
 
 util.inherits(hotukdeals, EventEmitter);
 
 hotukdeals.prototype.parse = function(html) {
-    $ = cheerio.load(html);
     var self = this;
-    $('.redesign-item-listing h2').each(function(i,el){
+    self._html = html;
+    self.$ = cheerio.load(html);
+    self.$('.redesign-item-listing h2').each(function(i,el){
         var item = {};
         item.title = $(this).text();
         self.items.push(item);
         self.emit("item-scraped",item);
     })
+}
+
+// where more items can be scraped from. To be called after items on 'start_url'
+// have been scraped.
+hotukdeals.prototype.more = function() {
+    // in this case, determine which is the current page, and fetch the link
+    // to the next one
+    this.currentPage = parseInt(this.$('.redesign-pagination selected').text());
+    console.log('currentPage: ', currentPage);
+    var nextPage = this.currentPage++;
+    return this.baseUrl + '/?page=' + nextPage.toString();
 }
 
 module.exports = exports = hotukdeals;
