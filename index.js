1ca606470d57e042ff3a517739598b917958a9da
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-09-01 22:54:17 +0100

    update to use nextUrl

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var ee           = new EventEmitter();
 function hotukdeals(){
     EventEmitter.call(this);
     this.name = 'hotukdeals.com';
-    this.baseUrl = 'http://wwww.hotukdeals.com';
+    this.baseUrl = 'http://www.hotukdeals.com';
 
     // set inside `parse`
     this.items = [];
     this._html = null;
     this.$ = null;
-    this.currentPage = null;
+    this.currentPage = 1;
 };
 
 util.inherits(hotukdeals, EventEmitter);
@@ -23,21 +23,24 @@ hotukdeals.prototype.parse = function(html) {
     self.$ = cheerio.load(html);
     self.$('.redesign-item-listing h2').each(function(i,el){
         var item = {};
-        item.title = $(this).text();
+        item.title = self.$(this).text();
         self.items.push(item);
         self.emit("item-scraped",item);
     })
 }
 
-// where more items can be scraped from. To be called after items on 'start_url'
-// have been scraped.
-hotukdeals.prototype.more = function() {
-    // in this case, determine which is the current page, and fetch the link
-    // to the next one
-    this.currentPage = parseInt(this.$('.redesign-pagination selected').text());
-    console.log('currentPage: ', currentPage);
-    var nextPage = this.currentPage++;
-    return this.baseUrl + '/?page=' + nextPage.toString();
+// Get the url to the next page
+hotukdeals.prototype.nextUrl = function() {
+    var self = this;
+    if(self.currentPage === null) {
+        self.currentPage = 1;
+        return self.baseUrl;
+    } else {
+//        this.currentPage = parseInt(this.$('.redesign-pagination selected').text());
+        console.log('currentPage: ', self.currentPage);
+        self.currentPage = self.currentPage+1;
+        return this.baseUrl + '/?page=' + self.currentPage.toString();
+    }
 }
 
 module.exports = exports = hotukdeals;
